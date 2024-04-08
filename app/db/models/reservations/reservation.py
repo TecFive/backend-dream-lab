ec4fbd93afd23a143f320b5caa32a68a9cacf95b
Patricio <patovw@gmail.com>
2024-04-08 11:17:10 -0600

    Created Azure SQL connection

--- app/db/models/reservations/reservation.py
+++ app/db/models/reservations/reservation.py
@@ -1,8 +1,19 @@
+import time
+from datetime import datetime
+from typing import Any
+
 from pydantic import BaseModel
 
 
 class Reservation(BaseModel):
     id: str
+    user_id: str
+    room_id: str
+    start_date: datetime
+    end_date: datetime
+    status: int
+    comments: str
+    updated_at: str
 
     def __init__(self, **data: Any):
         super().__init__(**data)
@@ -10,5 +21,12 @@ class Reservation(BaseModel):
     @staticmethod
     def create_from_persistence(user_persistence) -> "Reservation":
         return Reservation(
-            id=user_persistence["_id"]
+            id=user_persistence["_id"],
+            user_id=user_persistence["user_id"],
+            room_id=user_persistence["room_id"],
+            start_date=user_persistence["start_date"],
+            end_date=user_persistence["end_date"],
+            status=user_persistence["status"],
+            comments=user_persistence["comments"],
+            updated_at=user_persistence["updated_at"],
         )
--- app/db/repositories/reservations/reservation_repository.py
+++ app/db/repositories/reservations/reservation_repository.py
@@ -1,3 +1,91 @@
+from app.db.client import get_conn
+from app.db.models.reservations.reservation import Reservation
+
+
 # All static methods
 class ReservationRepository:
-    pass
+    @staticmethod
+    def get_all_reservations():
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"SELECT * FROM Reservations"
+        )
+
+        reservations = cursor.fetchall()
+        connection.close()
+
+        return reservations
+
+    @staticmethod
+    def get_reservations_by_user_id(user_id: str):
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"SELECT * FROM Reservations WHERE id_usuario = '{user_id}'"
+        )
+
+        reservations = cursor.fetchall()
+        connection.close()
+
+        return reservations
+
+    @staticmethod
+    def get_reservation_by_id(reservation_id: str):
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"SELECT * FROM Reservations WHERE id = '{reservation_id}'"
+        )
+
+        reservation = cursor.fetchone()
+        connection.close()
+
+        return reservation
+
+    @staticmethod
+    def create_reservation(reservation: Reservation):
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"INSERT INTO Reservations (id, user_id, room_id, start_date, end_date, status, comments, updated_at) "
+            f"VALUES ('{reservation.id}', '{reservation.user_id}', '{reservation.room_id}', '{reservation.start_date}', "
+            f"'{reservation.end_date}', '{reservation.status}', '{reservation.comments}', '{reservation.updated_at}')"
+        )
+
+        connection.commit()
+        connection.close()
+
+        return reservation
+
+    @staticmethod
+    def delete_reservation(reservation_id: str):
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"DELETE FROM Reservations WHERE id = '{reservation_id}'"
+        )
+
+        connection.commit()
+        connection.close()
+
+    @staticmethod
+    def update_reservation(reservation_id: str, reservation: Reservation):
+        connection = get_conn()
+        cursor = connection.cursor()
+
+        cursor.execute(
+            f"UPDATE Reservations SET id = '{reservation.id}', user_id = '{reservation.user_id}', room_id = '{reservation.room_id}', "
+            f"start_date = '{reservation.start_date}', end_date = '{reservation.end_date}', status = '{reservation.status}', "
+            f"comments = '{reservation.comments}', updated_at = '{reservation.updated_at}' WHERE id = '{reservation_id}'"
+        )
+
+        connection.commit()
+        connection.close()
+
+        return reservation
